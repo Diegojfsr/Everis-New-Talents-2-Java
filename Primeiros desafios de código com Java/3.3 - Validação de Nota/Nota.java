d457b0d33c2aedbdc3b3c2e4334315df743a22c1
Diego Jefferson <diegojfsr@gmail.com>
2021-11-11 12:32:11 -0300

    Primeiros-desafios-codigo-java

---
+++ Primeiros desafios de código com Java/3.3 - Validação de Nota/Nota.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Nota {
+	public static void main(String[] args) throws Exception {
+	  Scanner leitor = new Scanner(System.in);
+    double somaDasNotas = 0;
+    int contador = 0;
+    
+    do {
+      double notaEntradaConsole = leitor.nextDouble();
+
+      if (notaEntradaConsole < 0 || notaEntradaConsole > 10) {
+        System.out.println("nota invalida");
+      } else {
+        contador++;
+        somaDasNotas += notaEntradaConsole; 
+      }
+    } while (contador < 2);
+    
+    System.out.printf("media = " + String.format("%.2f", somaDasNotas / 2));
+  }
+}
